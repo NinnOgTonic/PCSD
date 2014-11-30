1bbe4679ac597ac95b66f36edbc956552b162bcc
Mathias Svensson <idolf@pwnies.dk>
2014-11-30 20:08:30 +0100

    loldongs is now initialized

--- Ass2/acertainbookstore/src/com/acertainbookstore/business/ConcurrentCertainBookStore.java
+++ Ass2/acertainbookstore/src/com/acertainbookstore/business/ConcurrentCertainBookStore.java
@@ -14,6 +14,7 @@ import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import com.acertainbookstore.interfaces.BookStore;
 import com.acertainbookstore.interfaces.StockManager;
@@ -32,6 +33,7 @@ public class ConcurrentCertainBookStore implements BookStore, StockManager {
     public ConcurrentCertainBookStore() {
         // Constructors are not synchronized
         bookMap = new HashMap<Integer, BookStoreBook>();
+        loldongs = new ReentrantReadWriteLock(true);
     }
 
     public void addBooks(Set<StockBook> bookSet)
