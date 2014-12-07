d4fe9ec30075edfe10417c4bd1b25bc03e8f4a43
Mathias Svensson <idolf@pwnies.dk>
2014-12-07 16:04:37 +0100

    BookSetGenerator

--- Ass3/acertainbookstore/src/com/acertainbookstore/client/workloads/BookSetGenerator.java
+++ Ass3/acertainbookstore/src/com/acertainbookstore/client/workloads/BookSetGenerator.java
@@ -1,7 +1,12 @@
 package com.acertainbookstore.client.workloads;
 
 import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.List;
 
+import com.acertainbookstore.business.ImmutableStockBook;
 import com.acertainbookstore.business.StockBook;
 
 /**
@@ -10,28 +15,50 @@ import com.acertainbookstore.business.StockBook;
  */
 public class BookSetGenerator {
 
-	public BookSetGenerator() {
-		// TODO Auto-generated constructor stub
-	}
+    public BookSetGenerator() {
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * Returns num randomly selected isbns from the input set
-	 *
-	 * @param num
-	 * @return
-	 */
-	public Set<Integer> sampleFromSetOfISBNs(Set<Integer> isbns, int num) {
-		return null;
-	}
+    /**
+     * Returns num randomly selected isbns from the input set
+     *
+     * @param num
+     * @return
+     */
+    public Set<Integer> sampleFromSetOfISBNs(Set<Integer> isbns, int num) {
+        Set<Integer> res = new HashSet<Integer>();
+        List<Integer> isbnsL = new ArrayList<Integer>(isbns);
+        Random rand = new Random();
 
-	/**
-	 * Return num stock books. For now return an ImmutableStockBook
-	 *
-	 * @param num
-	 * @return
-	 */
-	public Set<StockBook> nextSetOfStockBooks(int num) {
-		return null;
-	}
+        for(int totalCount = isbnsL.size(); totalCount > 0 && num > 0; totalCount--, num--) {
+            int nextIndex = rand.nextInt(totalCount);
+            int nextISBN  = isbnsL.get(nextIndex);
+            int lastISBN  = isbnsL.get(totalCount-1);
+            res.add(nextISBN);
+            isbnsL.set(nextIndex, lastISBN);
+        }
+
+        return res;
+    }
+
+    /**
+     * Return num stock books. For now return an ImmutableStockBook
+     *
+     * @param num
+     * @return
+     */
+    public Set<StockBook> nextSetOfStockBooks(int num) {
+        Set<StockBook> res = new HashSet<StockBook>();
+        Random rand = new Random();
+
+        for(int n = 0; n < num; n++) {
+            int ISBN = 1234 + rand.nextInt(1000);
+            String title = "AbstractSingletonProxyFactoryBean" + ISBN;
+            String author = "Adolf Hitler";
+            res.add(new ImmutableStockBook(ISBN, title, author, 1 + rand.nextInt(1000), 1 + rand.nextInt(200), 0, 0, 0, rand.nextBoolean()));
+        }
+
+        return res;
+    }
 
 }
