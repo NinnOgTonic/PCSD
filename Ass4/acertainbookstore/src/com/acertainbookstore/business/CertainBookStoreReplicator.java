a8c644a1ecb12d0f6e88624ada0929e0afcb0a35
Ceryn <git@ceryn.me>
2014-12-15 14:54:43 +0100

    .

--- Ass4/acertainbookstore/src/com/acertainbookstore/business/CertainBookStoreReplicator.java
+++ Ass4/acertainbookstore/src/com/acertainbookstore/business/CertainBookStoreReplicator.java
@@ -11,15 +11,59 @@ import com.acertainbookstore.interfaces.Replicator;
  * concurrently.
  */
 public class CertainBookStoreReplicator implements Replicator {
+    protected HttpClient client;
 
-	public CertainBookStoreReplicator(int maxReplicatorThreads) {
-		// TODO:Implement this constructor
-	}
+    public CertainBookStoreReplicator(int maxReplicatorThreads) {
+        // TODO:Implement this constructor
+        //setServerAddress(serverAddress);
+        client = new HttpClient();
+        client.setConnectorType(HttpClient.CONNECTOR_SELECT_CHANNEL);
+        client.setMaxConnectionsPerAddress(BookStoreClientConstants.CLIENT_MAX_CONNECTION_ADDRESS); // max
+        // concurrent
+        // connections
+        // to
+        // every
+        // address
+        //client.setThreadPool(new QueuedThreadPool(BookStoreClientConstants.CLIENT_MAX_THREADSPOOL_THREADS)); // max
+        client.setThreadPool(new QueuedThreadPool(maxReplicatorThreads)); // max
+        // threads
+        client.setTimeout(BookStoreClientConstants.CLIENT_MAX_TIMEOUT_MILLISECS); // seconds
+        // timeout;
+        // if
+        // no
+        // server
+        // reply,
+        // the
+        // request
+        // expires
+        client.start();
+    }
 
-	public List<Future<ReplicationResult>> replicate(Set<String> slaveServers,
-			ReplicationRequest request) {
-		// TODO: Implement this method
-		return null;
-	}
+    public List<Future<ReplicationResult>> replicate(Set<String> slaveServers,
+            ReplicationRequest request) {
+        // TODO: Implement this method
+        for(Sting slave : slaveServers) {
+            ContentExchange exchange = new ContentExchange();
+            String urlString = slave + "/" + request.messageType;
+
+            String xmlString = BookStoreUtility.serializeObjectToXMLString(request.dataSet);
+            exchange.setMethod("POST");
+            exchange.setURL(urlString);
+            Buffer requestContent = new ByteArrayBuffer(listISBNsxmlString);
+            exchange.setRequestContent(requestContent);
+
+            BookStoreUtility.SendAndRecv(this.client, exchange);
+        }
+        return null;
+    }
+
+    public void stop() {
+        try {
+            client.stop();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
